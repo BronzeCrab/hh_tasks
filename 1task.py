eb552576c79a5b26175b4003188815168bec69e2
BronzeCrab <gr4k@bk.ru>
2014-09-15 18:21:24 +0400

    Update 1task.py

--- 1task.py
+++ 1task.py
@@ -3,30 +3,13 @@
 # Медиана 
 # python 2.7
 
-print 'Введите элементы массива через пробел'
+print 'Введите элементы массивов через пробел'
 
-init_array = list(map(int, raw_input().split()))
-N = len (init_array)
+init_array = list(map(float, raw_input().split()))
+N = len (init_array) / 2
 
-init_array1 = []
-init_array2 = []
-
-for i in range (N):
-	print 'Введите %d элемент первого массива:' % i
-	init_array1.append(float(raw_input()))
-for i in range (N):
-	print 'Введите %d элемент втрого массива:' % i
-	init_array2.append(float(raw_input()))
-
-whole_array=[]
-
-for i in range (N):
-	
-	whole_array.append(init_array1[i])
-	whole_array.append(init_array2[i])
-
-whole_array.sort()
-median = (whole_array[N-1]+whole_array[N])/2
+init_array.sort()
+median = (init_array[N-1] + init_array[N]) / 2
 	
 print 'Медиана массива длиной 2N %r' % median
 
